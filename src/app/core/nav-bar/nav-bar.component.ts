f986b88bb69d281644a54ccca822f378138a729f
Mohamed Sameh <mosameh764@gmail.com>
2023-03-11 15:14:15 +0200

    upload the intial project

---
+++ src/app/core/nav-bar/nav-bar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrls: [
+    './nav-bar.component.css',
+    '../../../assets/css/style.css',
+    '../../../assets/css/responsive.css',
+  ],
+})
+export class NavBarComponent {}
