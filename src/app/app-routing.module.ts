f6b01c70f174417bb20b863ad91b7ea489ed3417
Ahmed Turky <Ahmedturky75@gmail.com>
2023-03-18 18:36:40 +0200

    cruds and filter for employee

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,32 +1,25 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import {RouterModule,Routes} from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { AddClinicComponent } from './Modules/clinic/add-clinic/add-clinic.component';
 import { ClinicListComponent } from './Modules/clinic/clinic-list/clinic-list.component';
+import { EmployeeListComponent } from './Modules/employee/employee-list/employee-list.component';
 import { AddMedicineComponent } from './Modules/medicine/add-medicine/add-medicine.component';
 import { EditMedicineComponent } from './Modules/medicine/edit-medicine/edit-medicine.component';
 import { ListMedicineComponent } from './Modules/medicine/list-medicine/list-medicine.component';
 
-
-
 const routes: Routes = [
-  {path:"clinic",component:ClinicListComponent},
-  {path:"clinic/add",component:AddClinicComponent},
+  { path: 'clinic', component: ClinicListComponent },
+  { path: 'clinic/add', component: AddClinicComponent },
   { path: 'add-medicine', component: AddMedicineComponent },
   { path: 'edit-medicine', component: EditMedicineComponent },
   { path: 'list-medicine', component: ListMedicineComponent },
+  { path: 'Employee', component: EmployeeListComponent },
   // {path:"clinci/update/:id",component:u
-
-]
+];
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule,RouterModule.forRoot(routes)
-  ],
-  exports:[RouterModule]
+  imports: [CommonModule, RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-
-export class AppRoutingModule { 
-
-
-}
+export class AppRoutingModule {}
