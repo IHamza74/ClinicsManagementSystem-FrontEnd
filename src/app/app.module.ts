969b02a8b52bab67c6ec115f20d6ad2ccbd2022b
Mohamed Sameh <mosameh764@gmail.com>
2023-03-18 16:25:23 +0200

    adding crudes+pagnation+filter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { MedicineModule } from './Modules/medicine/medicine.module';
+import { HttpClientModule } from '@angular/common/http';
+import { Routes, RouterModule } from '@angular/router';
+import { AddMedicineComponent } from './Modules/medicine/add-medicine/add-medicine.component';
+import { EditMedicineComponent } from './Modules/medicine/edit-medicine/edit-medicine.component';
+import { ListMedicineComponent } from './Modules/medicine/list-medicine/list-medicine.component';
+
+const medicineRoutes: Routes = [
+  { path: 'add-medicine', component: AddMedicineComponent },
+  { path: 'edit-medicine', component: EditMedicineComponent },
+  { path: 'list-medicine', component: ListMedicineComponent },
+];
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule,CoreModule],
+  imports: [
+    BrowserModule,
+    CoreModule,
+    MedicineModule,
+    HttpClientModule,
+    RouterModule.forRoot(medicineRoutes),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
