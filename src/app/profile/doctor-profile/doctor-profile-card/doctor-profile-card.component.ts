714123c03a37d70a80fb1cca01a15665b06d1196
Mostafa <mostafaelnady1997@gmail.com>
2023-03-20 12:42:16 +0200

    last edit

--- src/app/profile/doctor-profile/doctor-profile-card/doctor-profile-card.component.ts
+++ src/app/profile/doctor-profile/doctor-profile-card/doctor-profile-card.component.ts
@@ -4,7 +4,7 @@ import { Doctor } from 'src/app/Models/doctor';
 @Component({
   selector: 'app-doctor-profile-card',
   templateUrl: './doctor-profile-card.component.html',
-  styleUrls: ['./doctor-profile-card.component.css'],
+  styleUrls: ['./doctor-profile-card.component.css','./../../profile/profile.component.css'],
 })
 export class DoctorProfileCardComponent {
   @Input() doctor: Doctor;
