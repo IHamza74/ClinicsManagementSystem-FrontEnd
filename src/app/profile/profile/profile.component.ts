0905c94ed48930d7ec3bb041b28b33f46cd17817
Mostafa <mostafaelnady1997@gmail.com>
2023-03-23 04:28:45 +0200

    commit

--- src/app/profile/profile/profile.component.ts
+++ src/app/profile/profile/profile.component.ts
@@ -16,8 +16,7 @@ export class ProfileComponent implements OnInit {
   role: string;
   id: string;
   user: Employee | Doctor | Patient;
-
-  //userService: EmployeeService | DoctorService;
+  editClick=false;
   constructor(
     private employeeService: EmployeeService,
     private doctorService: DoctorService,
@@ -27,47 +26,6 @@ export class ProfileComponent implements OnInit {
     this.role = this.profileService.role;
     this.id = this.profileService.id;
 
-    /* delete these lines after login */
-    // this.role = 'employee'; //emp
-    // this.id = '63e410b48004ee37f326d924'; //emp
-    // this.user = new Employee('ahmed', 'Ahmed@gmail.com', '12345678', 25, {
-    //   government: 'Egypt',
-    //   city: 'Tanta',
-    //   street: 'ElTer3a st',
-    //   building: '10',
-    // });
-    // this.role = 'doctor'; //doc
-    // this.id = '63e4e87211226c1452238226'; //doc
-    // this.user = new Doctor(
-    //   '63e4e87211226c1452238226',
-    //   'ahmed',
-    //   25,
-    //   'kaka',
-    //   'ahmed@gail.com',
-    //   '1234567989',
-    //   '',
-    //   '',
-    //   []
-    // );
-    // this.role = 'patient'; //patient
-    // this.id = '63e4e9cf59930fe8aca061aa'; //patient
-    // this.user = new Patient(
-    //   '',
-    //   '',
-    //   0,
-    //   '',
-    //   {
-    //     government: 'Egypt',
-    //     city: 'Tanta',
-    //     street: 'ElTer3a st',
-    //     building: '10',
-    //   },
-    //   [],
-    //   '',
-    //   '',
-    //   '',
-    //   ''
-    // );
   }
   ngOnInit(): void {
     // this.role="doctor";
