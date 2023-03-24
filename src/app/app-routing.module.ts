937a99001fa62d3615e7e500abfca86cbbd9a210
Mostafa <mostafaelnady1997@gmail.com>
2023-03-24 03:54:05 +0200

    final touch appointments

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,13 +8,11 @@ import { AddMedicineComponent } from './Modules/medicine/add-medicine/add-medici
 import { EditMedicineComponent } from './Modules/medicine/edit-medicine/edit-medicine.component';
 import { ListMedicineComponent } from './Modules/medicine/list-medicine/list-medicine.component';
 import { DoctorListComponent } from './doctor/doctor-list/doctor-list.component';
-import { ProfileModule } from './profile/profile.module';
 import { ProfileComponent } from './profile/profile/profile.component';
 import { InoviceListComponent } from './Modules/invoice/inovice-list/inovice-list.component';
 import { InvoicePaidComponent } from './Modules/invoice/invoice-paid/invoice-paid.component';
 import { InvoiceUnpaidComponent } from './Modules/invoice/invoice-unpaid/invoice-unpaid.component';
 import { PatientListComponent } from './Modules/Patient/patient-list/patient-list.component';
-import { PatientAddComponent } from './Modules/Patient/patient-add/patient-add.component';
 import { AuthComponent } from './shared/auth/auth.component';
 import { AddAppointmentComponent } from './Modules/appointment/add-appointment/add-appointment.component';
 import { NotFoundComponent } from './not-found/not-found.component';
@@ -32,7 +30,7 @@ const routes: Routes = [
   { path: 'patient', component: PatientListComponent },
   { path: 'profile', component: ProfileComponent },
   { path: 'auth', component: AuthComponent },
-  {path:'appointment/add',component: AddAppointmentComponent}
+  {path:'appointment/add',component: AddAppointmentComponent},
   { path: 'invoice', component: InoviceListComponent },
   { path: 'invoice/:id', component: InvoicePaidComponent },
   { path: 'invoice/faild/:id', component: InvoiceUnpaidComponent },
